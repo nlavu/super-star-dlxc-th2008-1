b73906c8a9cb663a667175629cac63202d63b75a
ngocha869 <ngocha869@gmail.com>
2011-05-28 03:51:37 +0000

    DonDatHang.cs

--- SuperStarProject/BUS/DonDatHang.cs
+++ SuperStarProject/BUS/DonDatHang.cs
@@ -126,6 +126,198 @@ namespace BUS
             return res;
         }
 
+        public int CapNhatTrangThaiDonDatHang()
+        {
+            int res;
+            try
+            {
+                List<SqlParameter> lstParams = new List<SqlParameter>();
+                lstParams.Add(new SqlParameter("@maddh", MaDDH));
+                lstParams.Add(new SqlParameter("@trangthai", TrangThai));
+
+                res = SqlDataAccessHelper.ExecuteNoneQuery("spCapNhatTrangThaiDonDatHang", lstParams);
+
+
+            }
+            catch (Exception e)
+            {
+                res = 0;
+                throw e;
+            }
+            return res;
+        }
+
+        public int CapNhatNgayNhanHangCuaDonDatHang()
+        {
+            int res;
+            try
+            {
+                List<SqlParameter> lstParams = new List<SqlParameter>();
+                lstParams.Add(new SqlParameter("@maddh", MaDDH));
+                lstParams.Add(new SqlParameter("@ngaynhanhang", NgayNhanHang));
+
+                res = SqlDataAccessHelper.ExecuteNoneQuery("spCapNhatNgayNhanHangCuaDDH", lstParams);
+
+
+            }
+            catch (Exception e)
+            {
+                res = 0;
+                throw e;
+            }
+            return res;
+        }
+
+        public List<DonDatHang> LayDSDonDatHangTheoMaSanPham()
+        {
+            List<DonDatHang> lstDonDatHang = new List<DonDatHang>();
+
+            try
+            {
+                List<SqlParameter> lstParams = new List<SqlParameter>();
+                lstParams.Add(new SqlParameter("@masanpham", MaSanPham));
+                DataTable dtDonDatHang = new DataTable();
+                dtDonDatHang = SqlDataAccessHelper.ExecuteQuery("spLayDSDonDatHangTheoMaSanPham", lstParams);
+
+                foreach (DataRow dtRow in dtDonDatHang.Rows)
+                {
+                    DonDatHang donDatHang = new DonDatHang();
+                    donDatHang.MaDDH = int.Parse(dtRow["MaDDH"].ToString());
+                    donDatHang.NgayDat = DateTime.Parse(dtRow["NgayDat"].ToString());
+                    donDatHang.SoLuongDat = int.Parse(dtRow["SoLuongDat"].ToString());
+                    donDatHang.TrangThai = int.Parse(dtRow["TrangThai"].ToString());
+                    donDatHang.MaKhachHang = int.Parse(dtRow["MaKhachHang"].ToString());
+                    donDatHang.MaSanPham = int.Parse(dtRow["MaSanPham"].ToString());
+                    donDatHang.MaDaiLy = int.Parse(dtRow["MaDaiLy"].ToString());
+                    donDatHang.NgayNhanHang = DateTime.Parse(dtRow["NgayNhanHang"].ToString());
+                    donDatHang.ThanhTien = float.Parse(dtRow["MaDDH"].ToString());
+
+                    lstDonDatHang.Add(donDatHang);
+                    
+                }
+
+            }
+            catch (Exception e)
+            {
+               
+                throw e;
+            }
+            return lstDonDatHang;
+        }
+
+        public List<DonDatHang> LayDSDonDatHangTheoMaKhachHang()
+        {
+            List<DonDatHang> lstDonDatHang = new List<DonDatHang>();
+
+            try
+            {
+                List<SqlParameter> lstParams = new List<SqlParameter>();
+                lstParams.Add(new SqlParameter("@makhachang", MaKhachHang));
+                DataTable dtDonDatHang = new DataTable();
+                dtDonDatHang = SqlDataAccessHelper.ExecuteQuery("spLayDSDonDatHangTheoMaKhachHang", lstParams);
+
+                foreach (DataRow dtRow in dtDonDatHang.Rows)
+                {
+
+                    DonDatHang donDatHang = new DonDatHang();
+                    donDatHang.MaDDH = int.Parse(dtRow["MaDDH"].ToString());
+                    donDatHang.NgayDat = DateTime.Parse(dtRow["NgayDat"].ToString());
+                    donDatHang.SoLuongDat = int.Parse(dtRow["SoLuongDat"].ToString());
+                    donDatHang.TrangThai = int.Parse(dtRow["TrangThai"].ToString());
+                    donDatHang.MaKhachHang = int.Parse(dtRow["MaKhachHang"].ToString());
+                    donDatHang.MaSanPham = int.Parse(dtRow["MaSanPham"].ToString());
+                    donDatHang.MaDaiLy = int.Parse(dtRow["MaDaiLy"].ToString());
+                    donDatHang.NgayNhanHang = DateTime.Parse(dtRow["NgayNhanHang"].ToString());
+                    donDatHang.ThanhTien = float.Parse(dtRow["MaDDH"].ToString());
+
+                    lstDonDatHang.Add(donDatHang);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            return lstDonDatHang;
+        }
+
+        public List<DonDatHang> LayDSDonDatHangTheoMaDaiLyNhanHang()
+        {
+            List<DonDatHang> lstDonDatHang = new List<DonDatHang>();
+
+            try
+            {
+                List<SqlParameter> lstParams = new List<SqlParameter>();
+                lstParams.Add(new SqlParameter("@madaily", MaDaiLy));
+                DataTable dtDonDatHang = new DataTable();
+                dtDonDatHang = SqlDataAccessHelper.ExecuteQuery("spLayDSDonDatHangTheoMaDaiLy", lstParams);
+
+                foreach (DataRow dtRow in dtDonDatHang.Rows)
+                {
+
+                    DonDatHang donDatHang = new DonDatHang();
+                    donDatHang.MaDDH = int.Parse(dtRow["MaDDH"].ToString());
+                    donDatHang.NgayDat = DateTime.Parse(dtRow["NgayDat"].ToString());
+                    donDatHang.SoLuongDat = int.Parse(dtRow["SoLuongDat"].ToString());
+                    donDatHang.TrangThai = int.Parse(dtRow["TrangThai"].ToString());
+                    donDatHang.MaKhachHang = int.Parse(dtRow["MaKhachHang"].ToString());
+                    donDatHang.MaSanPham = int.Parse(dtRow["MaSanPham"].ToString());
+                    donDatHang.MaDaiLy = int.Parse(dtRow["MaDaiLy"].ToString());
+                    donDatHang.NgayNhanHang = DateTime.Parse(dtRow["NgayNhanHang"].ToString());
+                    donDatHang.ThanhTien = float.Parse(dtRow["MaDDH"].ToString());
+
+                    lstDonDatHang.Add(donDatHang);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            return lstDonDatHang;
+        }
+
+        public DonDatHang LayDSDonDatHangTheoMaDDH()
+        {
+            DonDatHang donDatHang = new DonDatHang();
+
+            try
+            {
+                List<SqlParameter> lstParams = new List<SqlParameter>();
+                lstParams.Add(new SqlParameter("@maddh", maDDH));
+                DataTable dtDonDatHang = new DataTable();
+                dtDonDatHang = SqlDataAccessHelper.ExecuteQuery("spLayDSDonDatHangTheoMaDDH", lstParams);
+
+                foreach (DataRow dtRow in dtDonDatHang.Rows)
+                {
+
+                    
+                    donDatHang.MaDDH = int.Parse(dtRow["MaDDH"].ToString());
+                    donDatHang.NgayDat = DateTime.Parse(dtRow["NgayDat"].ToString());
+                    donDatHang.SoLuongDat = int.Parse(dtRow["SoLuongDat"].ToString());
+                    donDatHang.TrangThai = int.Parse(dtRow["TrangThai"].ToString());
+                    donDatHang.MaKhachHang = int.Parse(dtRow["MaKhachHang"].ToString());
+                    donDatHang.MaSanPham = int.Parse(dtRow["MaSanPham"].ToString());
+                    donDatHang.MaDaiLy = int.Parse(dtRow["MaDaiLy"].ToString());
+                    donDatHang.NgayNhanHang = DateTime.Parse(dtRow["NgayNhanHang"].ToString());
+                    donDatHang.ThanhTien = float.Parse(dtRow["MaDDH"].ToString());
+
+                    
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            return donDatHang;
+        }
         public List<DonDatHang> LayDSDonDatHangTheoNgayDat(DateTime ngayBatDau, DateTime ngayKetThuc)
         {
             List<DonDatHang> lstDonDatHang = new List<DonDatHang>();
@@ -157,7 +349,7 @@ namespace BUS
             }
             catch (Exception e)
             {
-               
+
                 throw e;
             }
             return lstDonDatHang;
